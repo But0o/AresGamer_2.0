d8c88323ee123926b88471a19f5c4b43686b6b55
Ignacio Sandes <Sandesignacio05@gmail.com>
2026-09-16 20:38:11 -0300

    login + main

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 from funciones.menu import mostrar_menu, pedir_opcion
 from funciones.juegos import mostrar_juegos, cargar_juegos
+from funciones.login import login
+
 
 TITULO = "===== AresGamer 2.0 ====="
 JUEGOS = cargar_juegos()
@@ -21,7 +23,20 @@ def menu_inicial():
         opcion = pedir_opcion(opciones_inicio)
 
         if opcion == "1":
-            print("\n[Pendiente] Login.")
+            cuentas = cargar_cuentas()
+            usuario_ingresado = input("\nUsuario: ").strip()
+            contraseña_ingresada = input("Contraseña: ").strip
+            cuenta = login(usuario_ingresado, contraseña_ingresada, cuentas)
+
+            if cuenta is None:
+                print("Usuario o contrasñea incorrectos.")
+            elif cuenta["tipo"] == "Admin":
+                print("\nBienvenido, administrador " + cuenta["Usuario"] + ".")
+            elif cuenta["tipo"] == "pc":
+                print("\nBienvenido, " + cuenta["usuario"] + ".")
+            else:
+                print("Error tu cuenta no tiene un rol asignado, consulta con el administrador.")
+                
         elif opcion == "2":
             print("\n[Pendiente] Busqueda de juegos disponibles.")
         elif opcion == "3":
